b8a8a0dd094a40c73fc4183406f0c33549aad020
librame <pengxin@live.cn>
2019-07-13 11:40:04 +0800

    6.0.819.201

--- tests/Librame.Extensions.Data.EntityFrameworkCore.Tests/TestIdentifierService.cs
+++ tests/Librame.Extensions.Data.EntityFrameworkCore.Tests/TestIdentifierService.cs
@@ -6,26 +6,14 @@ namespace Librame.Extensions.Data.Tests
 {
     using Core;
 
-    /// <summary>
-    /// 测试标识符服务。
-    /// </summary>
     public class TestIdentifierService : IdentifierServiceBase
     {
-        /// <summary>
-        /// 构造一个 <see cref="TestIdentifierService"/> 实例。
-        /// </summary>
-        /// <param name="loggerFactory">给定的 <see cref="ILoggerFactory"/>。</param>
         public TestIdentifierService(ILoggerFactory loggerFactory)
             : base(loggerFactory)
         {
         }
 
 
-        /// <summary>
-        /// 异步获取文章标识。
-        /// </summary>
-        /// <param name="cancellationToken">给定的 <see cref="CancellationToken"/>（可选）。</param>
-        /// <returns>返回 <see cref="string"/>。</returns>
         public virtual Task<string> GetArticleIdAsync(CancellationToken cancellationToken = default)
         {
             return cancellationToken.RunFactoryOrCancellationAsync(() =>
